35ca56e7a29a1119581c44ec55cfb66de212f55e
Riaan <riaan@catch.co.nz>
2015-05-18 11:27:38 +1200

    option select radio buttons and dropdowns

--- code/SSMasterLibFieldList.php
+++ code/SSMasterLibFieldList.php
@@ -11,43 +11,55 @@ class SSMasterLibFieldList extends Extension {
 		$inline_fields = Config::inst()->get('SSMasterLibForm','inline_fields');
 
 		foreach($this->owner as $f) {
+			if (is_object($f)) {
 
-			if($f instanceof TextField || $f instanceof EmailField  || $f instanceof PasswordField ) {
-				$f->addPlaceholder($f->Title());
-			}
-
-			if(isset($this->ignores[$f->getName()])) continue;
-
-			if($f instanceof CompositeField) {
-				$f->getChildren()->SSMasterLibify();
-				continue;
-			}
-
-			if(!in_array($f->class, $inline_fields )) {
-				$f->addExtraClass('form-control');
-			}
-
-			$template = "SSMasterLib{$f->class}_holder";
-			if(SSViewer::hasTemplate($template)) {
-				$f->setFieldHolderTemplate($template);
-			}
-			else {
-				$f->setFieldHolderTemplate("SSMasterLibFieldHolder");
-			}
-
-			foreach(array_reverse(ClassInfo::ancestry($f)) as $className) {
-				$SSMasterLibCandidate = "SSMasterLib{$className}";
-				$nativeCandidate = $className;
-				if(SSViewer::hasTemplate($SSMasterLibCandidate)) {
-					$f->setTemplate($SSMasterLibCandidate);
-					break;
-				}
-				elseif(SSViewer::hasTemplate($nativeCandidate)) {
-					$f->setTemplate($nativeCandidate);
-					break;
+				if($f instanceof TextField || $f instanceof EmailField  || $f instanceof PasswordField ) {
+					$f->addPlaceholder($f->Title());
 				}
 
+				if(isset($this->ignores[$f->getName()])) continue;
 
+				// if ($f instanceof SelectionGroup_Item) {
+				// 	$f->addExtraClass('radio');
+				// 	//die(print_r($f->getAttributes(),1));
+				// }
+
+				if($f instanceof CompositeField) {
+					$f->getChildren()->SSMasterLibify();
+					continue;
+				}
+
+				if ($f instanceof DropdownField) {
+					$f->addExtraClass('catch-dropdown');
+					$f->setAttribute('data-placeholder', 'Please select one');
+				}
+
+				if(!in_array($f->class, $inline_fields )) {
+					$f->addExtraClass('form-control');
+				}
+
+				$template = "SSMasterLib{$f->class}_holder";
+				if(SSViewer::hasTemplate($template)) {
+					$f->setFieldHolderTemplate($template);
+				}
+				else {
+					$f->setFieldHolderTemplate("SSMasterLibFieldHolder");
+				}
+
+				foreach(array_reverse(ClassInfo::ancestry($f)) as $className) {
+					$SSMasterLibCandidate = "SSMasterLib{$className}";
+					$nativeCandidate = $className;
+					if(SSViewer::hasTemplate($SSMasterLibCandidate)) {
+						$f->setTemplate($SSMasterLibCandidate);
+						break;
+					}
+					elseif(SSViewer::hasTemplate($nativeCandidate)) {
+						$f->setTemplate($nativeCandidate);
+						break;
+					}
+
+
+				}
 			}
 		}
 
--- templates/SSMasterLibOptionsetField.ss
+++ templates/SSMasterLibOptionsetField.ss
@@ -1,11 +1,11 @@
-       
+
         <% if $Options.Count %>
             <% loop $Options %>
-                <div class="radio <% if $Top.Inline %>inline<% end_if %>">
-                    <label>
-                        <input id="$ID" class="radio" name="$Name" type="radio" value="$Value"<% if $isChecked %> checked<% end_if %><% if $isDisabled %> disabled<% end_if %>>
-                        $Title
-                    </label>
+                <div class="<% if $Top.Inline %>inline<% end_if %>">
+                  <input id="$ID" class="radio" name="$Name" type="radio" value="$Value"<% if $isChecked %> checked<% end_if %><% if $isDisabled %> disabled<% end_if %>>
+                  <label class="radio" for="$ID">                     
+                      $Title
+                  </label>
                 </div>
             <% end_loop %>
         <% else %>
