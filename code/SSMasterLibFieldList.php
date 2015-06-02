56f9385ec40a669630d0e5159f9b7f890d29500c
Aaron Latham-Ilari <breaks.nz@gmail.com>
2015-06-02 14:36:07 +1200

    ss forms fixes

--- code/SSMasterLibFieldList.php
+++ code/SSMasterLibFieldList.php
@@ -4,87 +4,86 @@
 class SSMasterLibFieldList extends Extension {
 
 
-	protected $ignores = array ();
+    protected $ignores = array ();
 
 
-	public function SSMasterLibify() {
-		$inline_fields = Config::inst()->get('SSMasterLibForm','inline_fields');
+    public function SSMasterLibify() {
+        $inline_fields = Config::inst()->get('SSMasterLibForm','inline_fields');
 
-		foreach($this->owner as $f) {
-			if (is_object($f)) {
+        foreach($this->owner as $f) {
+            if (is_object($f)) {
 
-				if($f instanceof TextField || $f instanceof EmailField  || $f instanceof PasswordField || $f instanceof TextareaField) {
-					$f->addPlaceholder($f->Title());
-				}
+                if($f instanceof TextField || $f instanceof EmailField  || $f instanceof PasswordField || $f instanceof TextareaField) {
+                    $f->addPlaceholder($f->Title());
+                }
 
-				if(isset($this->ignores[$f->getName()])) continue;
+                if(isset($this->ignores[$f->getName()])) continue;
 
-				if ($f instanceof SelectionGroup) {
-					$f->addLabelClass('radio');
-				}
+                if ($f instanceof SelectionGroup) {
+                    $f->addLabelClass('radio');
+                }
 
-				if($f instanceof CompositeField) {
-					if ($f instanceof FieldGroup) {
-						$template = "SSMasterLib{$f->class}_holder";
-						$this->setFieldHolderTemplate($template,$f);
-						$this->setTemplate($f);
-					}
-					$f->getChildren()->SSMasterLibify();
-					continue;
-				}
+                if($f instanceof CompositeField) {
+                    if ($f instanceof FieldGroup) {
+                        $template = "SSMasterLib{$f->class}_holder";
+                        $this->setFieldHolderTemplate($template,$f);
+                        $this->setTemplate($f);
+                    }
+                    $f->getChildren()->SSMasterLibify();
+                    continue;
+                }
 
-				if ($f instanceof DropdownField) {
-					$f->addExtraClass('catch-dropdown');
-					$f->setAttribute('data-placeholder', 'Please select one');
-				}
+                if ($f instanceof DropdownField) {
+                    $f->addExtraClass('catch-dropdown');
+                    if (!$f->getAttribute('data-placeholder'))
+                        $f->setAttribute('data-placeholder', 'Please select one');
+                }
 
+                if(!in_array($f->class, $inline_fields )) {
+                    $f->addExtraClass('form-control');
+                }
 
+                $template = "SSMasterLib{$f->class}_holder";
+                $this->setFieldHolderTemplate($template,$f);
 
-				if(!in_array($f->class, $inline_fields )) {
-					$f->addExtraClass('form-control');
-				}
+                $this->setTemplate($f);
+            }
+        }
 
-				$template = "SSMasterLib{$f->class}_holder";
-				$this->setFieldHolderTemplate($template,$f);
+        return $this->owner;
 
-				$this->setTemplate($f);
-			}
-		}
-
-		return $this->owner;
-
-	}
-
-
-	public function setFieldHolderTemplate($template,$formField) {
-		if(SSViewer::hasTemplate($template)) {
-			return $formField->setFieldHolderTemplate($template);
-		}
-		else {
-			return $formField->setFieldHolderTemplate("SSMasterLibFieldHolder");
-		}
-	}
-
-	public function setTemplate($formField) {
-		foreach(array_reverse(ClassInfo::ancestry($formField)) as $className) {
-			$SSMasterLibCandidate = "SSMasterLib{$className}";
-			$nativeCandidate = $className;
-			if(SSViewer::hasTemplate($SSMasterLibCandidate)) {
-				return $formField->setTemplate($SSMasterLibCandidate);
-				break;
-			}
-			elseif(SSViewer::hasTemplate($nativeCandidate)) {
-				$formField->setTemplate($nativeCandidate);
-				break;
-			}
-		}
-	}
+    }
 
 
+    public function setFieldHolderTemplate($template,$formField) {
+        if(SSViewer::hasTemplate($template)) {
+            return $formField->setFieldHolderTemplate($template);
+        }
+        else {
+            return $formField->setFieldHolderTemplate("SSMasterLibFieldHolder");
+        }
+    }
 
-	public function SSMasterLibIgnore($field) {
-		$this->ignores[$field] = true;
+    public function setTemplate($formField) {
+        foreach(array_reverse(ClassInfo::ancestry($formField)) as $className) {
+            $SSMasterLibCandidate = "SSMasterLib{$className}";
+            $nativeCandidate = $className;
+            if(SSViewer::hasTemplate($SSMasterLibCandidate)) {
+                return $formField->setTemplate($SSMasterLibCandidate);
+                break;
+            }
+            elseif(SSViewer::hasTemplate($nativeCandidate)) {
+                $formField->setTemplate($nativeCandidate);
+                break;
+            }
+        }
+    }
 
-		return $this->owner;
-	}
+
+
+    public function SSMasterLibIgnore($field) {
+        $this->ignores[$field] = true;
+
+        return $this->owner;
+    }
 }
--- templates/SSMasterLibDoubleLabelDropdownField.ss
+++ templates/SSMasterLibDoubleLabelDropdownField.ss
@@ -1,7 +1,9 @@
 <label class="label-1" for="$ID">$Label1</label>
 <label class="label-2 sublabel" for="$ID">$Label2</label>
-<select $AttributesHTML>
-<% loop $Options %>
-	<option value="$Value.XML"<% if $Selected %> selected="selected"<% end_if %><% if $Disabled %> disabled="disabled"<% end_if %>>$Title.XML</option>
-<% end_loop %>
+<select $AttributesHTML
+        data-validate-errors-container="#errors-$ID">
+    <% loop $Options %>
+        <option value="$Value.XML"<% if $Selected %> selected="selected"<% end_if %><% if $Disabled %> disabled="disabled"<% end_if %>>$Title.XML</option>
+    <% end_loop %>
 </select>
+<div id="errors-$ID"></div>
