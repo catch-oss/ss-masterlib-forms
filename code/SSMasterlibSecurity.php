1064e46c32715bb8fac9e16c37da986f71e8664c
Az Ilari <az@dev-io.us>
2018-06-11 14:20:43 +1200

    tabs

--- code/SSMasterlibSecurity.php
+++ code/SSMasterlibSecurity.php
@@ -10,29 +10,57 @@ class SSMasterLibSecurity extends Security {
      */
     protected function generateLoginFormSet($forms) {
 
-        // Include resources
-        Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery/jquery.js');
-        Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery-ui/jquery-ui.js');
-        Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
-        Requirements::css(THIRDPARTY_DIR . '/jquery-ui-themes/smoothness/jquery-ui.css');
-        Requirements::css(FRAMEWORK_DIR . '/css/Security_login.css');
-        Requirements::javascript(FRAMEWORK_DIR . '/javascript/TabSet.js');
+        // // Include resources
+        // Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery/jquery.js');
+        // Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery-ui/jquery-ui.js');
+        // Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
+        // Requirements::css(THIRDPARTY_DIR . '/jquery-ui-themes/smoothness/jquery-ui.css');
+        // Requirements::css(FRAMEWORK_DIR . '/css/Security_login.css');
+        // Requirements::javascript(FRAMEWORK_DIR . '/javascript/TabSet.js');
 
-        $content = '<div id="Form_EditForm">';
-        $content .= '<div class="ss-tabset test">';
-        $content .= '<ul>';
+        $content = '';
         $contentForms = '';
 
+        $i = 0;
         foreach($forms as $form) {
-            $content .= "<li><a href=\"#{$form->FormName()}_tab\">"
-                . $form->getAuthenticator()->get_name()
-                . "</a></li>\n";
+            $content .= '
+                <li class="c-small-tabs__tab"
+                    id="' . $form->FormName() . '-tab"
+                    aria-controls="' . $form->FormName() . '-panel"
+                    aria-selected="' . ($i == 0 ? 'true' : 'false') . '"
+                    role="tab">
+                    <span class="c-small-tabs__label">
+                        ' . $form->getAuthenticator()->get_name() . '
+                    </span>
+                </li>
+            ';
 
-            $contentForms .= '<div class="tab" id="' . $form->FormName() . '_tab">'
-                . $form->forTemplate() . "</div>\n";
+            $contentForms .= '
+                <div id="' . $form->FormName() . '-panel"
+                    aria-labelledby="' . $form->FormName() . '-tab"
+                    role="tabpanel"
+                    aria-hidden="' . ($i == 0 ? 'false' : 'true') . '">
+                    ' . $form->forTemplate() . '
+                </div>
+            ';
+
+            $i++;
         }
 
-        $content .= "</ul>\n" . $contentForms . "\n</div>\n</div>\n";
-        return $content;
+        $content = '
+            <div class="c-small-tabs__scroll-area js-focusable">
+                <div class="c-small-tabs__inner">
+                    <ul  class="c-small-tabs__list" role="tablist">
+                        ' . $content . '
+                    </ul>
+                </div>
+            </div>
+        ';
+
+        return '
+            <div id="Form_EditForm" class="c-small-tabs">
+                ' . $content . $contentForms . '
+            </div>
+        ';
     }
 }
