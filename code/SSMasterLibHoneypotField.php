4a6f47efe45dea8336436e8da01239d4b7f57a6c
Az Ilari <az@dev-io.us>
2021-09-01 09:26:23 +1200

    honey pot

--- code/SSMasterLibHoneypotField.php
+++ code/SSMasterLibHoneypotField.php
@@ -2,59 +2,61 @@
 
 namespace CatchDesign\SSMasterLibForms;
 
-use StudioBonito\SilverStripe\SpamProtection\Honeypot\FormField\HoneypotField;
+use Heyday\SilverStripe\HoneyPot\HoneyPotField;
 use SilverStripe\Forms\FormField;
 
 class SSMasterLibHoneypotField extends HoneypotField
 {
-    /**
-     * The number of seconds before you can submit a valid request.
-     *
-     * @var int
-     */
-    private static $time_limit = 5;
+    // for studio bonito
 
-    /**
-     * Create the Captcha Field.
-     *
-     * @codeCoverageIgnore
-     *
-     * @return string
-     */
-    protected function createHoneypotField()
-    {
-        return FormField::create_tag(
-            'input',
-            array(
-                'type'      => 'text',
-                'id'        => $this->ID(),
-                'name'      => $this->getName(),
-                'value'     => $this->Value(),
-                'style'     => 'display:none!important',
-                'class'     => 'no-label'
-            )
-        );
-    }
+    // /**
+    //  * The number of seconds before you can submit a valid request.
+    //  *
+    //  * @var int
+    //  */
+    // private static $time_limit = 5;
 
-    /**
-     * Create the Timestamp Field.
-     *
-     * @codeCoverageIgnore
-     *
-     * @return string
-     */
-    protected function createTimestampField()
-    {
-        return FormField::create_tag(
-            'input',
-            array(
-                'type'      => 'text',
-                'id'        => $this->ID() . '_Timestamp',
-                'name'      => $this->getName() . '_Timestamp',
-                'value'     => time(),
-                'style'     => 'display:none!important',
-                'class'     => 'no-label'
-            )
-        );
-    }
+    // /**
+    //  * Create the Captcha Field.
+    //  *
+    //  * @codeCoverageIgnore
+    //  *
+    //  * @return string
+    //  */
+    // protected function createHoneypotField()
+    // {
+    //     return FormField::create_tag(
+    //         'input',
+    //         array(
+    //             'type'      => 'text',
+    //             'id'        => $this->ID(),
+    //             'name'      => $this->getName(),
+    //             'value'     => $this->Value(),
+    //             'style'     => 'display:none!important',
+    //             'class'     => 'no-label'
+    //         )
+    //     );
+    // }
+
+    // /**
+    //  * Create the Timestamp Field.
+    //  *
+    //  * @codeCoverageIgnore
+    //  *
+    //  * @return string
+    //  */
+    // protected function createTimestampField()
+    // {
+    //     return FormField::create_tag(
+    //         'input',
+    //         array(
+    //             'type'      => 'text',
+    //             'id'        => $this->ID() . '_Timestamp',
+    //             'name'      => $this->getName() . '_Timestamp',
+    //             'value'     => time(),
+    //             'style'     => 'display:none!important',
+    //             'class'     => 'no-label'
+    //         )
+    //     );
+    // }
 }
